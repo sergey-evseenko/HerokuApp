fb13beb560b0ba047e951524be58434cb80c0534
sergey-evseenko <evseenkosv@tut.by>
2020-02-16 11:57:42 +0300

    Test case for Inputs.

---
+++ src/test/java/Inputs.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class Inputs {
+    @Test
+    public void inputsByArrow() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http://the-internet.herokuapp.com/inputs");
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        WebElement element = webDriver.findElement(By.xpath("//*[@type='number']"));
+        element.sendKeys(Keys.ARROW_UP, Keys.ARROW_UP, Keys.ARROW_DOWN);
+
+        String value = element.getAttribute("value");
+        assertEquals(value, "1", "Неверное значение!");
+        webDriver.quit();
+    }
+}
