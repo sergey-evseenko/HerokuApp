c180b950c3545a75bd004425db262e2cbc129a5e
sergey-evseenko <evseenkosv@tut.by>
2020-02-19 01:49:55 +0300

    Fixed code formatting. Updated "List<WebElement".

--- src/test/java/Checkboxes.java
+++ src/test/java/Checkboxes.java
@@ -4,13 +4,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class Checkboxes {
+
     @Test
     public void checkboxesClicks() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
@@ -18,29 +19,23 @@ public class Checkboxes {
         browser.get("http://the-internet.herokuapp.com/checkboxes");
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        WebElement element1 = browser.findElement(By.xpath("//*[@type='checkbox']"));
+        WebElement checkBox1 = browser.findElement(By.xpath("//*[@type='checkbox']"));
 
-        boolean isSelect = element1.isSelected();
+        boolean isSelect = checkBox1.isSelected();
         assertFalse(isSelect, "Error. CheckBox1: Элемент выбран!");
-
-        element1.click();
-        isSelect = element1.isSelected();
+        checkBox1.click();
+        isSelect = checkBox1.isSelected();
         assertTrue(isSelect, "Error. CheckBox1: Элемент не выбран!");
 
-        ArrayList<WebElement> checkboxes = new ArrayList<WebElement>();
-        checkboxes.addAll(0, browser.findElements(By.xpath("//*[@type='checkbox']")));
+        List<WebElement> checkBoxes = browser.findElements(By.xpath("//*[@type='checkbox']"));
+        WebElement checkBox2 = checkBoxes.get(1);
 
-        WebElement element2 = checkboxes.get(1);
-
-        isSelect = element2.isSelected();
+        isSelect = checkBox2.isSelected();
         assertTrue(isSelect, "Error. CheckBox2: Элемент не выбран!");
-
-        element2.click();
-        isSelect = element2.isSelected();
+        checkBox2.click();
+        isSelect = checkBox2.isSelected();
         assertFalse(isSelect, "Error. CheckBox2: Элемент выбран!");
-
         browser.quit();
-
-
     }
+
 }
