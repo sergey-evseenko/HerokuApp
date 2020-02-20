d5ebff7519f6fb38d8349e2619029dcde89e1b65
sergey-evseenko <evseenkosv@tut.by>
2020-02-20 17:13:47 +0300

    Added "BeforeMethod" and "AfterMethod".

--- src/test/java/AddRemoveElements.java
+++ src/test/java/AddRemoveElements.java
@@ -2,19 +2,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
+
 public class AddRemoveElements {
+    WebDriver browser;
+
+    @BeforeMethod
+    public void setupBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        browser = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void browserQuit() {
+        browser.quit();
+    }
 
     @Test
     public void addTwoElements() {
-
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
         browser.get("http://the-internet.herokuapp.com/add_remove_elements/");
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -24,9 +36,9 @@ public class AddRemoveElements {
 
         List<WebElement> deleteButtons = browser.findElements(By.xpath("//*[@onclick='deleteElement()']"));
         deleteButtons.get(1).click();
-        List<WebElement> deleteButtons1 = browser.findElements(By.xpath("//*[@onclick='deleteElement()']"));
-        assertEquals(deleteButtons1.size(), 1, "Кол-во кнопок не равняется единице!");
-        browser.quit();
+        deleteButtons = browser.findElements(By.xpath("//*[@onclick='deleteElement()']"));
+        assertEquals(deleteButtons.size(), 1, "Кол-во кнопок не равняется единице!");
+
     }
 
 }
